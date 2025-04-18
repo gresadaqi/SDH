a01615314c2a02e2278ccd0a29058ce32d10c40e
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-18 22:23:40 +0200

    update RailFenceCipher

--- src/ciphers/RailFenceCipher.java
+++ src/ciphers/RailFenceCipher.java
@@ -2,41 +2,67 @@ package ciphers;
 
 public class RailFenceCipher {
 
-        public static String encrypt(String text, int rails) {
-            if (rails <= 1) return text;
+    public static String encrypt(String text, int rails) {
+        if (rails <= 1) return text;
 
 
-            StringBuilder[] rail = new StringBuilder[rails];
-            for (int i = 0; i < rails; i++) {
-                rail[i] = new StringBuilder();
+        StringBuilder[] rail = new StringBuilder[rails];
+        for (int i = 0; i < rails; i++) {
+            rail[i] = new StringBuilder();
+        }
+
+        int currentRail = 0;
+        boolean goingDown = true;
+
+        for (char c : text.toCharArray()) {
+            rail[currentRail].append(c);
+
+
+            if (currentRail == 0) {
+                goingDown = true;
+            } else if (currentRail == rails - 1) {
+                goingDown = false;
             }
 
-            int currentRail = 0;
-            boolean goingDown = true;
 
-            for (char c : text.toCharArray()) {
-                rail[currentRail].append(c);
+            currentRail += goingDown ? 1 : -1;
+        }
 
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb : rail) {
+            result.append(sb);
+        }
 
-                if (currentRail == 0) {
-                    goingDown = true;
-                } else if (currentRail == rails - 1) {
-                    goingDown = false;
-                }
+        return result.toString();
+    }
 
-                
-                currentRail += goingDown ? 1 : -1;
+    public static String decrypt(String cipher,int rails){
+        if(rails<=1){
+            return cipher;
+        }
+
+        boolean[][] railMatrix=new boolean[rails][cipher.length()];
+        int currentRail=0;
+        boolean goingDown=true;
+
+        for(int i=0; i< cipher.length(); i++){
+            railMatrix[currentRail][i]=true;
+
+            if(currentRail==0){
+                goingDown=true;
+            }else if (currentRail==rails -1){
+                goingDown=false;
             }
+            currentRail+=goingDown ? 1: -1 ;
 
-            StringBuilder result = new StringBuilder();
-            for (StringBuilder sb : rail) {
-                result.append(sb);
-            }
-
-            return result.toString();
         }
 
 
+
+
+
+        
+    }
 }
-            
+
 
